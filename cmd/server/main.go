74bf28a4dd3fde0e151ee2c86acf5bf8d2ae4cf9
Wanderson Vieira <root@DESKTOP-4ICGUCV>
2024-07-02 00:11:08 -0300

    fea: add swagger docs

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
 	"github.com/go-chi/jwtauth"
+	httpSwagger "github.com/swaggo/http-swagger"
 	"github.com/waanvieira/api-users/configs"
+	_ "github.com/waanvieira/api-users/docs"
 	"github.com/waanvieira/api-users/internal/entity"
 	databaseUser "github.com/waanvieira/api-users/internal/infra/database"
 	databaseProduct "github.com/waanvieira/api-users/internal/infra/database/product"
@@ -15,6 +18,20 @@ import (
 	"gorm.io/gorm"
 )
 
+// @title           Go Expert API Example
+// @version         1.0
+// @description     Product API with auhtentication
+// @termsOfService  http://swagger.io/terms/
+
+// @contact.name   Wanderson Vieira
+
+// @license.name   W
+
+// @host      localhost:8001
+// @BasePath  /
+// @securityDefinitions.apikey ApiKeyAuth
+// @in header
+// @name Authorization
 func main() {
 	// iniciando as nossas config
 	configs, err := configs.LoadConfig(".")
@@ -35,9 +52,10 @@ func main() {
 	r.Use(middleware.Logger)
 	// Esse middleware serve para se der algum problema, algum panic no sistema o sistema continuar a execução e não parar
 	// No caso quando acontece algum erro inesperado, algum panico a nossa aplicação ficaria fora do ar
+	// r.Use(LogOwner) // Aqui estamos utilizando o nosso próprio middleware dentro do ichi
 	r.Use(middleware.Recoverer)
 	r.Use(middleware.WithValue("jwt", configs.TokenAuth))
-	r.Use(middleware.WithValue("JwtExperesIn", configs.JwtExpiresIn))	
+	r.Use(middleware.WithValue("JwtExperesIn", configs.JwtExpiresIn))
 
 	// // Estamos iniciando a struct de "classe" indicando qual banco de dados vamos usar
 	productDB := databaseProduct.NewProduct(db)
@@ -79,9 +97,26 @@ func main() {
 
 	})
 
+	r.Get("/docs/*", httpSwagger.Handler(httpSwagger.URL("http://localhost:8001/docs/doc.json")))
 	r.Get("/test", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Teste"))
 	})
 
 	http.ListenAndServe(":8001", r)
+}
+
+// exemplo de um middleware próprio, seria um exmeplo de middleware para validar por exemplo ACL, verificar permissionamento de um usuário dependendo
+// Do seu perfil
+// Next -> seria apenas uma convenção usada para referenciar a variável recebida na request, seria um meio campo para acessar a nossa aplicação
+// nesse caso next recebe um handler http e retorna esse handler, na função podemos fazer alguma validação, alteração e ou retornar um erro
+// Direcionar para outra função ou middleware ou passar para frente essa requisição
+func LogOwner(next http.Handler) http.Handler {
+	// Nesse caso retornamos um handler como recebemos pegando uma resposta response e uma request (requisição)
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Nesse caso executamos uma ação na nossa aplicação que seria imprimir na tela o método e a URL que recebemos a requisição
+		// Nesse caos nó estamos pegando o contexto do usuário, podemos recuperar alguma informação no contexto com o middleware
+		log.Println(r.Method, r.URL.Path)
+		// Aqui damos continuidade na aplicação com next.ServeHTTP que é da própria linguagem
+		next.ServeHTTP(w, r)
+	})
 }
